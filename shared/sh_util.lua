812759d9fc9f052f1e6c73b50c3656bf41bde82c
Ilias Rbayti <63159154+Kenshiin13@users.noreply.github.com>
2023-05-13 12:32:00 +0200

    Added dumpTable util function

--- shared/sh_util.lua
+++ shared/sh_util.lua
@@ -174,3 +174,33 @@ end
 function VyHub.Util:timer_loop(delay, callback)
 	tmr.create():alarm(delay, tmr.ALARM_SINGLE, callback)
 end
+
+function VyHub.Util:dumpTable(table, nb)
+    if nb == nil then
+		nb = 0
+	end
+
+	if type(table) == 'table' then
+		local s = ''
+		for i = 1, nb + 1, 1 do
+			s = s .. "    "
+		end
+
+		s = '{\n'
+		for k,v in pairs(table) do
+			if type(k) ~= 'number' then k = '"'..k..'"' end
+			for i = 1, nb, 1 do
+				s = s .. "    "
+			end
+			s = s .. '['..k..'] = ' .. self:dumpTable(v, nb + 1) .. ',\n'
+		end
+
+		for i = 1, nb, 1 do
+			s = s .. "    "
+		end
+
+		return s .. '}'
+	else
+		return tostring(table)
+	end
+end
