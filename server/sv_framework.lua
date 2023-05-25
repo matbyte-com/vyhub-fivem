1a81afffa564cf21a0dcfa57f636c86a37c19775
Ilias Rbayti <63159154+Kenshiin13@users.noreply.github.com>
2023-05-25 15:36:10 +0200

    Added ESX Framework boiler plate

---
+++ server/sv_framework.lua
@@ -0,0 +1,26 @@
+VyHub.Framework = VyHub.Framework or {}
+
+function VyHub.Framework:getPlayerFromId(src)
+    if (VyHub.Config.framework == "ESX") then
+        return ESX.GetPlayerFromId(src)
+    end
+end
+
+function VyHub.Framework:getPlayerFromIdentifier(identifier)
+    if (VyHub.Config.framework == "ESX") then
+        return ESX.GetPlayerFromIdentifier(identifier)
+    end
+end
+
+function VyHub.Framework:getPlayerGroup(src)
+    local xPlayer = VyHub.Framework:getPlayerFromId(src)
+    if (VyHub.Config.framework == "ESX") then
+        return xPlayer.getGroup()
+    end
+end
+
+function VyHub.Framework:getPlayers()
+    if(VyHub.Config.framework == "ESX") then
+        return ESX.GetExtendedPlayers()
+    end
+end
