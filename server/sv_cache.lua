de11b6e17d9056e6889897018c234b0f05a3e6e7
Ilias Rbayti <63159154+Kenshiin13@users.noreply.github.com>
2023-06-01 13:59:04 +0200

    Added Cache System for server

---
+++ server/sv_cache.lua
@@ -0,0 +1,45 @@
+VyHub.Cache = VyHub.Cache or {}
+
+function VyHub.Cache:save(key, value)
+    local resourcePath = GetResourcePath(GetCurrentResourceName())
+    local data = {
+        timestamp = os.time(),
+        data = value
+    }
+
+    local filename = string.format("%s/%s.json", resourcePath, key)
+    local json = json.encode(data)
+
+    VyHub:msg("Write " .. filename .. ": " .. json, "debug")
+
+    local file = io.open(filename, "w")
+    if (file) then
+        file:write(json)
+        file:close()
+    else
+
+    end
+end
+
+function VyHub.Cache:get(key, max_age)
+    local data_str = LoadResourceFile(GetCurrentResourceName(), f("%s.json", key))
+
+    if (not data_str) then
+        return nil
+    end
+
+    local data = json.decode(data_str)
+    if (not data) then
+        return nil
+    end
+
+    if (type(data) == "table" and data.timestamp and data.data) then
+        if (max_age ~= nil and os.time() - data.timestamp > max_age) then
+            return nil
+        end
+
+        return data.data
+    end
+
+    return nil
+end
